2a2e7b9aedc1915c2ca6335c917cad09f9dbc5e5
AlinPMacovei <macoveialinp@gmail.com>
2015-10-01 15:29:53 +0300

    Homepage done

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
+	<div class="motto">
+		<p>Orasul Respira</p>
+		<p>Fotografie</p>
+	</div>
 
+	<div class="circle">
+		<div class="camera">
+			<img src="img/camera.png">
+		</div>
+		<div class="fiveD">
+			<p>Inscrie o poza si poti</p>
+			<p>castiga un <span>5d MarkIII</span></p>
+		</div>
+	</div>
+
+	<div class="rules">
+		<a href="">Regulament</a>
+	</div>
 @endsection
 
 @section('javascript')
