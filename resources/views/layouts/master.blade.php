ed9f255a878ca48f35f5ee3a84e6db22faf14171
Luca Daniel <lucadanielcostin@gmail.com>
2015-11-26 10:55:24 +0200

    Fix background image on smaller screens

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,16 +14,16 @@
 		<link rel="stylesheet" href="/css/main.css">
 		<link rel="stylesheet" href="/css/style.css">
 
-        @yield('css')
+		@yield('css')
 
-        @yield('fbTags')
+		@yield('fbTags')
 	</head>
 
 	<body >
-        <div style="position: fixed; width: 100%; height: 100%; top:0; left: 0; background: rgba(0,0,0,0.6); z-index: 9998; display: none;" id="overlay"></div>
+		<div style="position: fixed; width: 100%; height: 100%; top:0; left: 0; background: rgba(0,0,0,0.6); z-index: 9998; display: none;" id="overlay"></div>
 
 		<div class="wrapper {{$selectedPage==0 ? 'wrapper1' : 'wrapper2'}}">
-			<div class="layer"></div>		
+			<div class="layer"></div>
 			<div class="content">
 				<div class="header">
 					@include('partials.header')
@@ -33,13 +33,13 @@
 					@yield('content')
 				</div>
 
-                @if ($selectedPage==0)
+				@if ($selectedPage==0)
 				<div class="footer">
-                    <div style="width: 740px; margin-left: auto; margin-right: auto;">
+					<div style="width: 740px; margin-left: auto; margin-right: auto;">
 					@include('partials.footer')
-                    </div>
+					</div>
 				</div>
-                @endif
+				@endif
 			</div>
 		</div>
 
@@ -51,62 +51,64 @@
 		<script src="/js/main.js"></script>
 
 		@yield('javascript')
-		
+
 		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 		<script>
-            /*
+			/*
 			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
 			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
 			e.src='https://www.google-analytics.com/analytics.js';
 			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
 			ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-              */
+			  */
 
-            $(document).ready(function()
-            {
-                console.log('master');
-                var mainWrapper = $(".wrapper").first();
-                var mainLayer = $(".layer").first();
-                var mainContent = $(".content").first();
+			$(document).ready(function()
+			{
+				/*
+				console.log('master');
+				var mainWrapper = $(".wrapper").first();
+				var mainLayer = $(".layer").first();
+				var mainContent = $(".content").first();
 
-                function scaleBG(){
-                    var ws = $(window).width();
-                    var hs = $(window).height();
-                    var wi = 2048;
-                    var hi = 1313;
-                    var rs = ws/rs;
-                    var ri = wi/ri;
+				function scaleBG(){
+					var ws = $(window).width();
+					var hs = $(window).height();
+					var wi = 2048;
+					var hi = 1313;
+					var rs = ws/rs;
+					var ri = wi/ri;
 
-                    var wnew = 0;
-                    var hnew = 0;
-                    if (rs>ri){
-                        wnew = wi * hs/hi;
-                        hnew = hs;
-                    }
-                    else{
-                        wnew = ws;
-                        hnew = hi * ws/wi;
-                    }
+					var wnew = 0;
+					var hnew = 0;
+					if (rs>ri){
+						wnew = wi * hs/hi;
+						hnew = hs;
+					}
+					else{
+						wnew = ws;
+						hnew = hi * ws/wi;
+					}
 
-                    mainWrapper.css('backgroundSize',wnew+"px "+hnew+"px");
-                    mainWrapper.css('backgroundPosition',((ws-wnew)/2)+"px "+((hs-hnew)/2)+"px");
+					mainWrapper.css('backgroundSize',wnew+"px "+hnew+"px");
+					mainWrapper.css('backgroundPosition',((ws-wnew)/2)+"px "+((hs-hnew)/2)+"px");
 
-                    mainWrapper.width(ws);
-                    mainWrapper.height(hs);
+					mainWrapper.width(ws);
+					mainWrapper.height(hs);
 
-                    mainContent.width(ws);
-                    mainContent.height(hs);
+					mainContent.width(ws);
+					mainContent.height(hs);
 
-                    mainLayer.width(ws);
-                    mainLayer.height(hs);
-                }
-                scaleBG();
+					mainLayer.width(ws);
+					mainLayer.height(hs);
+				}
+				scaleBG();
 
-                $(window).resize(function(){
-                    scaleBG();
-                })
-            })
+				$(window).resize(function(){
+					scaleBG();
+				})
+				*/
+			})
 		</script>
 		<!-- /Scripts -->
 
