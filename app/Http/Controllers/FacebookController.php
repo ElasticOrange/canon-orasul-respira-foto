7f007e99288d2de7ac6fb3f39b16626a2eed0337
Luca Daniel <lucadanielcostin@gmail.com>
2015-11-27 14:57:37 +0200

    Testing login

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -13,103 +13,103 @@ use ImageIntervention;
 
 class FacebookController extends Controller
 {
-    //
-    public function getLogin()
-    {
-        Session::forget('profileId');
-        $fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
-        $data = array(
-            'selectedPage' => 2 ,
-            'fbLink' => $fb->getLoginUrl(['email'])
-        );
-        return view('register.fblogin',$data);
-    }
- 
-    public function getCallback()
-    {
-        $fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
-        // Obtain an access token.
-        try {
-            $token = $fb->getAccessTokenFromRedirect();
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
+	//
+	public function getLogin()
+	{
+		Session::forget('profileId');
+		$fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
+		$data = array(
+			'selectedPage' => 2 ,
+			'fbLink' => $fb->getLoginUrl(['email'])
+		);
+		return view('register.fblogin',$data);
+	}
 
-        // Access token will be null if the user denied the request
-        // or if someone just hit this URL outside of the OAuth flow.
-        if (! $token) {
-            // Get the redirect helper
-            $helper = $fb->getRedirectLoginHelper();
+	public function getCallback()
+	{
+		$fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
+		// Obtain an access token.
+		try {
+			$token = $fb->getAccessTokenFromRedirect();
+		} catch (Facebook\Exceptions\FacebookSDKException $e) {
+			dd($e->getMessage());
+		}
 
-            if (! $helper->getError()) {
-                abort(403, 'Unauthorized action.');
-            }
+		// Access token will be null if the user denied the request
+		// or if someone just hit this URL outside of the OAuth flow.
+		if (! $token) {
+			// Get the redirect helper
+			$helper = $fb->getRedirectLoginHelper();
 
-            // User denied the request
-            dd(
-                $helper->getError(),
-                $helper->getErrorCode(),
-                $helper->getErrorReason(),
-                $helper->getErrorDescription()
-            );
-        }
+			if (! $helper->getError()) {
+				abort(403, 'Unauthorized action.');
+			}
 
-        if (! $token->isLongLived()) {
-            // OAuth 2.0 client handler
-            $oauth_client = $fb->getOAuth2Client();
+			// User denied the request
+			dd(
+				$helper->getError(),
+				$helper->getErrorCode(),
+				$helper->getErrorReason(),
+				$helper->getErrorDescription()
+			);
+		}
 
-            // Extend the access token.
-            try {
-                $token = $oauth_client->getLongLivedAccessToken($token);
-            } catch (Facebook\Exceptions\FacebookSDKException $e) {
-                dd($e->getMessage());
-            }
-        }
+		if (! $token->isLongLived()) {
+			// OAuth 2.0 client handler
+			$oauth_client = $fb->getOAuth2Client();
 
-        $fb->setDefaultAccessToken($token);
+			// Extend the access token.
+			try {
+				$token = $oauth_client->getLongLivedAccessToken($token);
+			} catch (Facebook\Exceptions\FacebookSDKException $e) {
+				dd($e->getMessage());
+			}
+		}
 
-        // Save for later
-        Session::put('fb_user_access_token', (string) $token);
+		$fb->setDefaultAccessToken($token);
 
-        // Get basic info on the user from Facebook.
-        try {
-            $response = $fb->get('/me?fields=id,name,email,picture.type(large)');
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
+		// Save for later
+		Session::put('fb_user_access_token', (string) $token);
 
-        // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-        $facebook_user = $response->getGraphUser();
+		// Get basic info on the user from Facebook.
+		try {
+			$response = $fb->get('/me?fields=id,name,email,picture.type(large)');
+		} catch (Facebook\Exceptions\FacebookSDKException $e) {
+			dd($e->getMessage());
+		}
 
-        // Create the user if it does not exist or update the existing entry.
-        // This will only work if you've added the SyncableGraphNodeTrait to your User model.
-        $user = User::createOrUpdateGraphNode($facebook_user);
+		// Convert the response to a `Facebook/GraphNodes/GraphUser` collection
+		$facebook_user = $response->getGraphUser();
 
-        $arrContextOptions=array(
-            "ssl"=>array(
-                "verify_peer"=>false,
-                "verify_peer_name"=>false,
-            ),
-        );
+		// Create the user if it does not exist or update the existing entry.
+		// This will only work if you've added the SyncableGraphNodeTrait to your User model.
+		$user = User::createOrUpdateGraphNode($facebook_user);
 
-        $img = ImageIntervention::make(file_get_contents($user->url, false, stream_context_create($arrContextOptions)));
-        $img->fit(100);
-        $img->save( base_path() . '/public/images/profilePhotos/thumb_100_'. md5($user->id).'.jpg', 100);
+		$arrContextOptions=array(
+			"ssl"=>array(
+				"verify_peer"=>false,
+				"verify_peer_name"=>false,
+			),
+		);
 
-        $img = ImageIntervention::make(file_get_contents($user->url, false, stream_context_create($arrContextOptions)));
-        $img->fit(50);
-        $img->save( base_path() . '/public/images/profilePhotos/thumb_50_'. md5($user->id).'.jpg', 100);
+		$img = ImageIntervention::make(file_get_contents($user->url, false, stream_context_create($arrContextOptions)));
+		$img->fit(100);
+		$img->save( base_path() . '/public/images/profilePhotos/thumb_100_'. md5($user->id).'.jpg', 100);
 
-        // Log the user into Laravel
-        Auth::login($user);
+		$img = ImageIntervention::make(file_get_contents($user->url, false, stream_context_create($arrContextOptions)));
+		$img->fit(50);
+		$img->save( base_path() . '/public/images/profilePhotos/thumb_50_'. md5($user->id).'.jpg', 100);
 
-        //maybe this need to redirect back to the originating page
-        if (Session::has('profileId'))
-        {
-            return redirect('/profile/index/'.Session::get('profileId'));
-        }
-        else{
-            return redirect('/register');
-        }
-    }
+		// Log the user into Laravel
+		Auth::login($user);
+
+		//maybe this need to redirect back to the originating page
+		if (Session::has('profileId'))
+		{
+			return redirect('/profile/index/'.Session::get('profileId'));
+		}
+		else{
+			return redirect('/register');
+		}
+	}
 }
--- resources/views/register/fblogin.blade.php
+++ resources/views/register/fblogin.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
-<div style="margin-top: 220px;  width: 100%; text-align: center;">
-    <a class="red-button red-button-border" style="line-height: 30px;" href="{{$fbLink}}"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
-</div>
+	<div style="margin-top: 220px;  width: 100%; text-align: center;">
+		<a class="red-button red-button-border" style="line-height: 30px;" href="{{$fbLink}}" id="login-fb-js"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
+	</div>
 @endsection
 
 @section('javascript')
-
+<script>
+$(document).ready(function(){
+	$('#login-fb-js').click(function(){
+		FB.login(function(response) {
+			// handle the response
+		}, {scope: 'email'});
+	});
+});
+</script>
 @endsection
