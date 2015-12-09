ec8291c49cdda3dcb296cd672e41f072e79bb894
smooth-op <bara.catalin@gmail.com>
2015-12-09 19:31:01 +0200

    fixed fb login on register page

--- resources/views/register/fblogin.blade.php
+++ resources/views/register/fblogin.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div style="margin-top: 220px;  width: 100%; text-align: center;">
-		<a class="red-button red-button-border" style="line-height: 30px;" href="{{$fbLink}}" id="login-fb-js"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
+		<a class="red-button red-button-border" style="line-height: 30px;" id="login-fb-js"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
 	</div>
 @endsection
 
@@ -12,6 +12,7 @@ $(document).ready(function(){
 	$('#login-fb-js').click(function(){
 		FB.login(function(response) {
 			// handle the response
+            window.location.replace("/facebook/js-callback");
 		}, {scope: 'email'});
 	});
 });
