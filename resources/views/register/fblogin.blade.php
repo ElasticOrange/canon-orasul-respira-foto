dac0939b2a658fe082004fe8c4ae9dcf32fdc1c5
smooth-op <bara.catalin@gmail.com>
2015-12-09 18:26:44 +0200

    Removed href from fb login url

--- resources/views/register/fblogin.blade.php
+++ resources/views/register/fblogin.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div style="margin-top: 220px;  width: 100%; text-align: center;">
-		<a class="red-button red-button-border" style="line-height: 30px;" href="{{$fbLink}}" id="login-fb-js"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
+		<a class="red-button red-button-border" style="line-height: 30px;" id="login-fb-js"><img src="/img/fb-logo.png" height="30px"> Login with Facebook </a>
 	</div>
 @endsection
 
