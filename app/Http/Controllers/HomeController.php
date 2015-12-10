8b8ce49b0ef8e26870edecacdf6c557a386e91e5
Luca Daniel <lucadanielcostin@gmail.com>
2015-12-10 15:29:50 +0200

    Try cookie fix for safari

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,22 +4,31 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getIndex()
-	{
-		$data = array('selectedPage' => 0);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getIndex()
+    {
+        $data = array('selectedPage' => 0);
 
-		return view('home.index', $data);
-	}
+        return view('home.index', $data);
+    }
 
-	public function postIndex()
-	{
-		$data = array('selectedPage' => 0);
+    public function postIndex()
+    {
+        $data = array('selectedPage' => 0);
 
-		return view('home.index', $data);
-	}
+        if (count($_COOKIE) === 0) {
+            return '<script>top.location = "https://canon-pwp.elasticorange.com/cookiefix";</script>';
+        }
+
+        return view('home.index', $data);
+    }
+
+    public function getCookiefix()
+    {
+        return '<script>top.location = "https://www.facebook.com/canonromania/app/1059827974029585/";</script>';
+    }
 }
